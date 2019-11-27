74ec608bafc3fc07445f06bac0376b30c987c453
Micah Shackelford <micahdshackelford@gmail.com>
2019-11-27 14:23:16 -0500

    Began API integration for dynamic weather

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,38 @@ import React from 'react';
 import CurrentTemp from './Components/CurrentTemp';
 import AddLocation from './Components/AddLocation';
 import Forecast from './Components/Forecast';
+import API_KEY from './config/API_KEY.config';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentlocation : {},
+
+      currentLocation : {zip: '37912', data: {}},
       locations : []
     }
   }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData () {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${this.state.currentLocation.zip},us&APPID=${API_KEY}`, {
+      method: 'GET'
+    }).then((res) => {
+      return res.json();
+    }).then((res) => {
+      this.setState({
+        currentLocation: {zip:this.state.currentLocation.zip, data: res}
+      });
+    })
+  }
+
   render(){
     return (
       <div className="App">
-        <CurrentTemp />
+        <CurrentTemp data={this.state.currentLocation} />
         <AddLocation />
         <Forecast />
       </div>
--- client/src/Components/CurrentTemp.js
+++ client/src/Components/CurrentTemp.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 let CurrentTemp = (props) => {
+  let conditions = "";
+  if(props.data.data.weather) {
+    conditions = props.data.data.weather[0].main;
+  }
+  console.log(props.data)
   return (
     <div className="current-temp">
       <h1 className="temp">92&deg;F</h1>
-      <h2>Knoxville,TN 37912</h2>
+      <h2>{props.data.data.name} - {props.data.zip}</h2>
+      <p>{conditions} Skys</p>
     </div>
   )
 }
